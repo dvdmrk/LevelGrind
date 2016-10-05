3c59646e990930020214ba9542df3e9315cf3dd3
david merk <DavidMerk25@gmail.com>
2016-10-05 17:08:18 -0400

    Initial commit...

---
+++ LevelGrindSim/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LevelGrindSim.MvcApplication" Language="C#" %>
---
+++ LevelGrindSim/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LevelGrindSim.Startup))]
+namespace LevelGrindSim
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
